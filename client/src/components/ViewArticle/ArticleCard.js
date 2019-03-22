7a0438f9931494e1f203a66168e6cb479f419c56
TalArbatov <talarbatov98@gmail.com>
2019-03-22 14:48:35 +0200

    machine switch

--- client/src/components/ViewArticle/ArticleCard.js
+++ client/src/components/ViewArticle/ArticleCard.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-export default ({article}) => {
-    return(
-        <div>
-            <p>Title: {article.title}</p>
-            <p>Author: {article.author}</p>
-            <p>date created: {article.dateCreated}</p>
-            <p>content: {article.content}</p>
-        </div>
-    )
-}
+export default ({ article }) => {
+  return (
+    <div>
+      <p>Title: {article.title}</p>
+      <p>Author: {article.author}</p>
+      <p>date created: {article.dateCreated.toLocaleString()}</p>
+      <p>content: {article.content}</p>
+    </div>
+  );
+};
